e450b97df1b6189e6f238f322fc6c89ded67ed7f
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-06-17 14:52:34 +0530

    Create  Longest Increasing Subsequence(Algo).cpp.cpp

---
+++ Longest Increasing Subsequence(Algo).cpp.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    int lengthOfLIS(vector<int>& nums) {
+
+        int n=nums.size();
+
+        vector<int>dp(n,1);
+
+        int maxi=1;
+
+        for(int ind=0;ind<n;ind++){
+            for(int prev_ind=0;prev_ind<ind;prev_ind++){
+                if(nums[prev_ind]<nums[ind]){
+                    dp[ind]=max(dp[ind],1+dp[prev_ind]);
+                }
+            }
+
+            maxi=max(maxi,dp[ind]);
+        }
+
+        return maxi;
+        
+    }
+};
