1264b63b970670ac17c46f73db9b17bb330b7b65
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-07-19 10:52:13 +0530

    Update Remove Sub-Folders from the Filesystem.cpp

--- Remove Sub-Folders from the Filesystem.cpp
+++ Remove Sub-Folders from the Filesystem.cpp
@@ -1,1 +1,25 @@
+class Solution {
+public:
+    vector<string> removeSubfolders(vector<string>& folder) {
 
+        int n=folder.size();
+        sort(folder.begin(),folder.end());
+
+        vector<string>ans;
+        ans.push_back(folder[0]);
+
+        for(int i=1;i<n;i++){
+            string lastf=ans.back();
+            lastf+="/";
+
+            if(folder[i].substr(0,lastf.size())!=lastf){
+                ans.push_back(folder[i]);
+            }
+        }
+
+        return ans;
+
+
+        
+    }
+};
