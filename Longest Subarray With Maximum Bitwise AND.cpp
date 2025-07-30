27af0d0804f05c978b5bf698839ee099c54e0cc9
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-07-30 16:55:41 +0530

    Create Longest Subarray With Maximum Bitwise AND.cpp

---
+++ Longest Subarray With Maximum Bitwise AND.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+
+        int n=nums.size();
+        int ans=0;
+        int count=0;
+        int maxi=*max_element(nums.begin(),nums.end());
+
+        for(int i=0;i<n;i++){
+            if(nums[i]==maxi){
+                count++;
+                ans=max(ans,count);
+            }
+            else{
+                count=0;
+            }
+        }
+
+        return ans;
+
+        
+    }
+};
