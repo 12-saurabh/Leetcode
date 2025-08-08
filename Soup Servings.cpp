58839902f2e9d2caa128e0bc54a52c9ce207d8e4
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-08-08 12:49:34 +0530

    Update Soup Servings.cpp

--- Soup Servings.cpp
+++ Soup Servings.cpp
@@ -1,1 +1,38 @@
 
+class Solution {
+public:
+    double soupServings(int n) {
+
+        if(n>=5000){
+            return 1.0;
+        }
+
+        int N=(n+24)/25;
+
+        vector<vector<double>>dp(N+1,vector<double>(N+1,0.0));
+
+        dp[0][0]=0.5;
+
+        for(int i=1;i<=N;i++){
+            dp[0][i]=1.0;
+        }
+
+        for(int i=1;i<=N;i++){
+            dp[i][0]=0.0;
+        }
+
+        for(int a=1;a<=N;a++){
+            for(int b=1;b<=N;b++){
+
+                dp[a][b]=0.25*(dp[max(0,a-4)][b]+
+                               dp[max(0,a-3)][max(0,b-1)]+
+                               dp[max(0,a-2)][max(0,b-2)]+
+                               dp[max(0,a-1)][max(0,b-3)]);
+            }
+        }
+
+        return dp[N][N];
+
+        
+    }
+};
