3a12be10e96893b06918aebabf40b95f106ffe83
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-08-23 16:14:46 +0530

    Create Find Eventual Safe States.cpp

---
+++ Find Eventual Safe States.cpp
@@ -0,0 +1,54 @@
+class Solution {
+
+private:
+    
+    bool dfs(int node,vector<vector<int>>&adj,vector<int>&vis,vector<int>&pathVis,vector<int>&check){
+        vis[node]=1;
+        pathVis[node]=1;
+        check[node]=0;
+
+        for(auto i:adj[node]){
+            if(!vis[i]){
+                if(dfs(i,adj,vis,pathVis,check)==true){
+                    check[i]=0;
+                    return true;
+                }
+            }
+            else if(pathVis[i]){
+                check[i]=0;
+                return true;
+            }
+        }
+
+        check[node]=1;
+        pathVis[node]=0;
+
+        return false;
+    }
+public:
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        int V=graph.size();
+        
+
+        vector<int>vis(V,0);
+        vector<int>pathVis(V,0);
+
+        vector<int>check(V,0);
+
+        vector<int>ans;
+
+        for(int i=0;i<V;i++){
+            if(!vis[i]){
+                dfs(i,graph,vis,pathVis,check);
+            }
+        }
+
+        for(int i=0;i<V;i++){
+            if(check[i]==1){
+                ans.push_back(i);
+            }
+        }
+        
+        return ans;
+    }
+};
