25226fff815276968503c3bb2eeedbacac76256a
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-09-12 18:56:21 +0530

    Create Vowels Game in a String.cpp

---
+++ Vowels Game in a String.cpp
@@ -0,0 +1,13 @@
+
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        for(int i = 0; i < s.length(); i++){
+            char ch = s[i];
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'){
+                return true;
+            }
+        }
+        return false;
+    }
+};
