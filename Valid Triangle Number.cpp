273056ae3022b18a864e77766a8e72d43fbaafd8
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-09-26 12:21:04 +0530

    Create  Valid Triangle Number.cpp

---
+++ Valid Triangle Number.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+
+        int n=nums.size();
+
+        if(n<3){
+            return 0;
+        }
+
+        sort(nums.begin(),nums.end());
+
+        int count=0;
+
+        int k=n-1;
+        while(k>=0){
+            
+            int i=0;
+            int j=k-1;
+
+            while(i<j){
+                if(nums[i]+nums[j]>nums[k]){
+                    count+=(j-i);
+                    j--;
+                }
+                else{
+                    i++;
+                }
+            }
+            k--;
+        }
+
+        return count;
+
+
+
+
+        
+    }
+};
