3a1c4fa6433a44764e44468c3813f066cef1c0e4
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-10-01 12:40:25 +0530

    Create Water Bottles.cpp

---
+++ Water Bottles.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+
+        int ans=numBottles+(numBottles-1)/(numExchange-1);
+
+        return ans;
+
+        
+    }
+};
