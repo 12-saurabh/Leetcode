817a33f7da719c6d3c14da8edd3ec9772c1bf36f
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2025-12-10 13:44:34 +0530

    Create Count the Number of Computer Unlocking Permutations.cpp

---
+++ Count the Number of Computer Unlocking Permutations.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    
+    const int MOD=1e9+7;
+    int countPermutations(vector<int>& complexity) {
+
+        int n=complexity.size();
+
+        int f=complexity[0];
+        for(int i=1;i<n;i++){
+            if(complexity[i]<=f){
+                return 0;
+            }
+        }
+
+        long long  fact=1;
+        for(int i=2;i<n;i++){
+            fact=(fact*i)%MOD;
+        }
+
+        return fact;
+
+
+
+
+
+
+        
+    }
+};
