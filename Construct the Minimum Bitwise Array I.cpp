db9658dec280d5e08ffb8887cc2c434b3b0a29ce
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2026-01-20 19:05:35 +0530

    Create  Construct the Minimum Bitwise Array I.cpp

---
+++ Construct the Minimum Bitwise Array I.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> minBitwiseArray(vector<int>& nums) {
+        
+        for(auto &i:nums){
+
+            int res=-1;
+            int d=1;
+
+            while((i&d)!=0){
+                res=i-d;
+                d<<=1;
+            }
+
+            i=res;
+        }
+
+        return nums;
+
+        
+    }
+};
