71b5f73b944abc3ba41ba2bedbc228dbf228be2d
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2026-01-29 13:09:35 +0530

    Create Minimum Cost to Convert String I.cpp

---
+++ Minimum Cost to Convert String I.cpp
@@ -0,0 +1,71 @@
+class Solution {
+public:
+
+    void dijk(int src,vector<pair<int,int>>adj[],vector<int>&dist){
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
+
+        dist[src]=0;
+        pq.push({0,src});
+
+        while(!pq.empty()){
+            int d=pq.top().first;
+            int u=pq.top().second;
+
+            pq.pop();
+
+            if(d>dist[u]){
+                continue;
+            }
+
+            for(auto x:adj[u]){
+                int v=x.first;
+                int w=x.second;
+
+                if(dist[v]>d+w){
+                    dist[v]=d+w;
+                    pq.push({dist[v],v});
+                }
+            }
+        }
+    }
+    long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
+
+        if(source.size()!=target.size()){
+            return -1;
+        }
+
+        vector<pair<int,int>>adj[26];
+
+        for(int i=0;i<original.size();i++){
+            adj[original[i]-'a'].push_back({changed[i]-'a',cost[i]});
+        }
+
+        vector<vector<int>>dist(26,vector<int>(26,INT_MAX));
+
+        for(int i=0;i<26;i++){
+            dijk(i,adj,dist[i]);
+        }
+
+        long long ans=0;
+        for(int i=0;i<source.size();i++){
+            int u=source[i]-'a';
+            int v=target[i]-'a';
+            if(u==v){
+                continue;
+            }
+
+            if(dist[u][v]==INT_MAX){
+                return -1;
+            }
+
+            ans+=dist[u][v];
+        }
+
+        return ans;
+
+
+
+
+        
+    }
+};
