f78445622aeda6b4d9ab88a98c4edcd6bbdfd08f
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2026-02-21 10:15:38 +0530

    Create Prime Number of Set Bits in Binary Representation.cpp

---
+++ Prime Number of Set Bits in Binary Representation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int countPrimeSetBits(int left, int right) {
+        int count=0;
+
+        unordered_set<int>p={2,3,5,7,11,13,17,19};
+
+        for(int i=left;i<=right;i++){
+            int bit=__builtin_popcount(i);
+
+            if(p.find(bit)!=p.end()){
+                count++;
+            }
+        }
+
+        return count;
+    }
+};
