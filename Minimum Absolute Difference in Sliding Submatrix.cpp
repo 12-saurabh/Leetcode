214cda76d9d5c26502256e4c4c8360c96394f524
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2026-03-20 17:49:42 +0530

    Create Minimum Absolute Difference in Sliding Submatrix.cpp

---
+++ Minimum Absolute Difference in Sliding Submatrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> minAbsDiff(vector<vector<int>>& grid, int k) {
+
+        int m=grid.size();
+        int n=grid[0].size();
+
+        vector<vector<int>>ans(m-k+1,vector<int>(n-k+1,0));
+
+        for(int i=0;i<=m-k;i++){
+            for(int j=0;j<=n-k;j++){
+                vector<int>v;
+
+                for(int x=i;x<i+k;x++){
+                    for(int y=j;y<j+k;y++){
+                        v.push_back(grid[x][y]);
+                    }
+                }
+
+                sort(v.begin(),v.end());
+                v.erase(unique(v.begin(),v.end()),v.end());
+
+                if(v.size()<=1){
+                    ans[i][j]=0;
+                }
+                else{
+                    int mini=INT_MAX;
+                    for(int p=0;p<v.size()-1;p++){
+                        mini=min(mini,v[p+1]-v[p]);
+                    }
+
+                    ans[i][j]=mini;
+                }
+            }
+        }
+
+        return ans;
+
+
+        
+    }
+};
