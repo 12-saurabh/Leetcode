ff15a7cb612c1e39fc38ecb0c18a99528f59d4f8
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2026-04-15 10:28:35 +0530

    Create  Shortest Distance to Target String in a Circular Array.cpp

---
+++ Shortest Distance to Target String in a Circular Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int closestTarget(vector<string>& words, string target, int startIndex) {
+
+        int n=words.size();
+
+        for(int i=0;i<n;i++){
+            if(words[(startIndex-i+n)%n]==target || words[(startIndex+i)%n]==target){
+                return i;
+            }
+        }
+
+        return -1;
+        
+    }
+};
