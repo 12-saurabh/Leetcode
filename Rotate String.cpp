d2166c62176821d02a583b75f370d1a4f126412c
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2026-05-03 14:26:50 +0530

    Create Rotate String.cpp

---
+++ Rotate String.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+
+        if(s.length()!=goal.length()){
+            return false;
+        }
+
+        string ans=s+s;
+
+        if(ans.find(goal)!=string::npos){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+
+
+
+
+
+        
+    }
+};
