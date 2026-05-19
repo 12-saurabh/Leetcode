4c9d10b0f0ebfc37f8e84e6f4c9a25b7f486700a
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2026-05-19 10:49:15 +0530

    Create Minimum Common Value.cpp

---
+++ Minimum Common Value.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        
+        unordered_map<int,int>mp;
+
+        for(auto i:nums2){
+            mp[i]++;
+        }
+
+        for(int i=0;i<nums1.size();i++){
+            if(mp.find(nums1[i])!=mp.end()){
+                return nums1[i];
+            }
+        }
+
+        return -1;
+    }
+};
