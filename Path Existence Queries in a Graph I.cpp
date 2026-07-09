e9f153adec6e543ef0e76345d6fdd63d4322b1a1
12-saurabh <159817086+12-saurabh@users.noreply.github.com>
2026-07-09 13:41:53 +0530

    Create  Path Existence Queries in a Graph I.cpp

---
+++ Path Existence Queries in a Graph I.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<bool> pathExistenceQueries(int n, vector<int>& nums, int maxDiff, vector<vector<int>>& queries) {
+        
+        vector<int>comp(n,0);
+        int count=0;
+
+        for(int i=1;i<n;i++){
+            if(nums[i]-nums[i-1]>maxDiff){
+                count++;
+            }
+
+            comp[i]=count;
+        }
+
+        vector<bool>ans;
+
+        for(auto &q:queries){
+            ans.push_back(comp[q[0]]==comp[q[1]]);
+        }
+
+        return ans;
+
+    }
+};
